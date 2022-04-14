3563a3572d1bdb2a0bdd9a7b0c6e1e1e91663164
estesthebestes <esteshunter22@gmail.com>
2022-04-14 13:06:57 -0500

    I made it through to the end but not the bonus. Will need to add the background in order for it to be completed for this first step.

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(link, left, bottom) {
+    let obj = document.createElement('img');
+    obj.src = link;
+    obj.style.position = 'fixed';
+    obj.style.left = left + 'px';
+    obj.style.bottom = bottom + 'px';
+    document.body.append(obj);
+    return obj;
+}
+
+function newItem(link, left, bottom) {
+        let item = document.createElement('img');
+        item.src = link;
+        item.style.position = 'fixed';
+        item.style.left = left + 'px';
+        item.style.bottom = bottom + 'px'; 
+        item.addEventListener('dblclick', function() { item.remove(); });
+        document.body.append(item);
+        return item;
+  }
+// images go here
+newImage('./assets/pine-tree.png', 450, 200)
+newImage( './assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage( './assets/well.png', 500, 425)
+
+// items go here
+newItem('/assets/shield.png', 165, 185)
+newItem('/assets/sword.png', 500, 405)
+newImage('assets/staff.png', 600, 100)
+
+
+// this makes the sword disappear on click
+  
